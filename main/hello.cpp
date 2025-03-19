4fa8935fc229b0f667a5b971dc0091d4c3a1285b
Devansh Verma <130306114+devansh1109@users.noreply.github.com>
2025-03-19 10:53:30 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << "Making changes to see Jenkins console output \n";
   return 0;
 }
